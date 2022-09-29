c4cd2c28354a9d5a8c1b26f92af267f0644cfc9d
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-09-29 21:27:22 +0300

    - Add first Demo

---
+++ MVC-Intro-Demo/MVC-Intro-Demo/Controllers/ProductsController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro_Demo.Models;
+using System.Text;
+using System.Text.Json;
+
+namespace MVC_Intro_Demo.Controllers
+{
+    public class ProductsController : Controller
+    {
+      private  IEnumerable<ProductViewModel> products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 7.00
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Ham",
+                Price = 5.50
+            },
+            new ProductViewModel()
+            {
+                Id = 3,
+                Name = "Bread",
+                Price = 1.50
+            }
+
+        };
+
+        [HttpGet]
+        [ActionName("My-Products")]
+        public IActionResult All(string keyword)
+        {
+            if (keyword != null)
+            {
+                var selectedProducts = products.Where(p => p.Name.ToLower().Contains(keyword.ToLower()));
+                return View(selectedProducts);
+            }
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            ProductViewModel pr = products.FirstOrDefault(x => x.Id == id);
+
+            if (pr == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return View(pr);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAllAsJson()
+        {
+            var oprions = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            return Json(products, oprions);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllAsText()
+        {
+
+            return Content(GetProducts()); 
+        }
+
+        public IActionResult Download()
+        {
+            string products = GetProducts();
+
+            Response.Headers.Add("content-disposition", "attachment; filename=products.txt");
+
+           
+            return File(
+                Encoding.UTF8.GetBytes(products), "text/plain");
+        }
+
+        private string GetProducts()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var pr in products)
+            {
+                sb.AppendLine($"{pr.Id}. {pr.Name} - {pr.Price}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ MVC-Intro-Demo/MVC-Intro-Demo/Models/ProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Intro_Demo.Models
+{
+    public class ProductViewModel
+    {
+        /// <summary>
+        /// Id of product
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of product
+        /// </summary>
+        public string Name { get; set; } = null!;
+
+        public double Price { get; set; }
+    }
+}
