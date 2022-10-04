eb0b9d36ce6dd15eb6749ce02930c7f5494016c0
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-10-04 21:50:55 +0300

    - Create ChatApp - Implement ChatController - Implement view's

---
+++ ChatApp/ChatApp/Models/MessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Models
+{
+    public class MessageViewModel
+    {
+        [Required]
+        public string Sender { get; set; } = null!;
+
+        [Required]
+        public string MessageText { get; set; } = null!;
+    }
+}
