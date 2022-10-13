18f6bd2f422c0276153063a7bc89fff043751184
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-10-13 23:03:55 +0300

    1.Define Entities 2.Define the DbContext 3.Seed the Database

---
+++ ForumApp/ForumApp/Data/DataConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace ForumApp.Data
+{
+    public static class DataConstants
+    {
+        public static class Post
+        {
+            public const int TitleMin = 10;
+            public const int ContentMin = 10;
+
+            public const int TitleMax = 50;
+            public const int ContentMax = 1500; 
+        }
+
+        public static class PostAnswer
+        {
+            public const int AnswerMax = 50;
+
+            public const int AnswerMin = 2;
+        }
+    }
+}
---
+++ ForumApp/ForumApp/Data/Entities/Post.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ForumApp.Data.Entities
+{
+    public class Post
+    {
+        /// <summary>
+        /// Post id
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Title of post
+        /// </summary>
+        [Required]
+        [StringLength(DataConstants.Post.TitleMax, MinimumLength = DataConstants.Post.TitleMin)]
+        public string Title { get; set; } = null!;
+
+        /// <summary>
+        /// Content of post
+        /// </summary>
+        [Required]
+        [StringLength(DataConstants.Post.ContentMax, MinimumLength = DataConstants.Post.ContentMin)]
+        public string Content { get; set; } = null!;
+
+        /// <summary>
+        /// Boolean is deleted or not
+        /// </summary>
+        public bool IsActive { get; set; } = true;
+    }
+}
