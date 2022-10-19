3048b5e7df895177eb4fb18c0747ac8ae9e49699
Todor Vasilev <todorvasilev005@gmail.com>
2022-10-19 18:17:12 +0300

    login and register implemented

--- Exam-Preparation/Watchlist/Controllers/AccountController.cs
+++ Exam-Preparation/Watchlist/Controllers/AccountController.cs
@@ -1,12 +1,103 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Watchlist.Data;
+using Watchlist.Data.Entitites;
+using Watchlist.Models;
 
 namespace Watchlist.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly WatchlistDbContext data;
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public AccountController(
+            WatchlistDbContext context,
+            UserManager<User> _userManager,
+            SignInManager<User> _signInManager) 
         {
-            return View();
+            this.data = context; 
+            this.userManager = _userManager;
+            this.signInManager = _signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            var model = new RegisterViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            User user = new User()
+            {
+                Email = model.Email,
+                UserName = model.UserName
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, isPersistent: false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            var model = new LoginViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await userManager.FindByNameAsync(model.UserName);
+
+            if (user != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError("", "Invalid Login");
+
+            return View(model);
         }
     }
 }
